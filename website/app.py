8034ea231d4ba28f1f7c96e640fcdbbe5c0c590d
Alpha <GAS01574@gassouth.com>
2019-10-16 17:08:43 -0400

    up

--- website/app.py
+++ website/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 import queries
-import geojson
+# import geojson
 
 from flask import Flask, jsonify, render_template
 # from flask_sqlalchemy import SQLAlchemy
