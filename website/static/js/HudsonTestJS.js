c69e6c95704e958cd4bd316e87652e909cadcf01
hheatley <47985564+hheatley@users.noreply.github.com>
2019-10-20 10:22:16 -0400

    Hudson test pages

--- website/static/js/HudsonTestJS.js
+++
@@ -1,6 +0,0 @@
-function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/AAHAWebProject-Story2Only/Story2-Whereandhowbadisit?:retry=yes&:embed=y&:display_count=yes&:origin=viz_share_link";
-
-    var viz = new tableau.Viz(containerDiv, url);
-}
