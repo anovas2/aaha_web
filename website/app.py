5eb8b355ebce8974428cbd1a22048ae38c68ab28
anovas <novas.a@gmail.com>
2019-10-22 18:23:48 -0400

    update

--- website/app.py
+++ website/app.py
@@ -8,14 +8,13 @@ import queries
 # import geojson
 
 from flask import Flask, jsonify, render_template, url_for
+
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 # DB_PATH = os.path.join(PATH, r'aaha.db')
-
-
 
 
 #################################################
@@ -45,6 +44,8 @@ cursor = connection.cursor()
 #
 
 census_income_to_rent_dataset = queries.get_df(connection, 'census_income_to_rent_dataset')
+
+
 # census_dataset = queries.get_df(connection, 'census_dataset')
 # censustract = queries.get_df(connection, 'censustract')
 # hud_dataset = queries.get_df(connection, 'hud_dataset')
@@ -82,62 +83,85 @@ def years():
     return jsonify(list(years))
 
 
-
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
-
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
-
-
-@app.route("/census_income_to_rent_dataset/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-    # Sort by sample
-    sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+# @app.route("/metadata/<sample>")
+# def sample_metadata(sample):
+#     """Return the MetaData for a given sample."""
+#     sel = [
+#         Samples_Metadata.sample,
+#         Samples_Metadata.ETHNICITY,
+#         Samples_Metadata.GENDER,
+#         Samples_Metadata.AGE,
+#         Samples_Metadata.LOCATION,
+#         Samples_Metadata.BBTYPE,
+#         Samples_Metadata.WFREQ,
+#     ]
+#
+#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+#
+#     # Create a dictionary entry for each row of metadata information
+#     sample_metadata = {}
+#     for result in results:
+#         sample_metadata["sample"] = result[0]
+#         sample_metadata["ETHNICITY"] = result[1]
+#         sample_metadata["GENDER"] = result[2]
+#         sample_metadata["AGE"] = result[3]
+#         sample_metadata["LOCATION"] = result[4]
+#         sample_metadata["BBTYPE"] = result[5]
+#         sample_metadata["WFREQ"] = result[6]
+#
+#     print(sample_metadata)
+#     return jsonify(sample_metadata)
 
 
-@app.route('/story1.html')
+# @app.route("/census_income_to_rent_dataset/<sample>")
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(Samples).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
+#
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+#
+#     # Sort by sample
+#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
+#
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
+
+
+@app.route('/story1/')
 def story1():
     return render_template('story1.html')
+
+
+@app.route('/story2/')
+def story2():
+    return render_template('story2.html')
+
+
+@app.route('/story3/')
+def story3():
+    return render_template('story3.html')
+
+
+@app.route('/story4/')
+def story4():
+    return render_template('story4.html')
+
+
+@app.route('/data/')
+def data():
+    return render_template('data.html')
+
+@app.route('/team/')
+def team():
+    return render_template('team.html')
 
 
 if __name__ == "__main__":
